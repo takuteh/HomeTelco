ccaaa4fb38b7385a1f94638e7c246a8e56c5f89c
takuteh <takumi.ktym@gmail.com>
2026-02-15 23:02:20 +0900

    reconciliation_loopを追加

--- tc_reconciler.py
+++ tc_reconciler.py
@@ -2,15 +2,27 @@ import tc_executer
 import get_filter_db
 import get_filter_tc
 import compare_ac
+import time
 
-def main():
+INTERVAL = 30
+
+def reconciliation_loop():
+    while True:
+        try:
+            reconciliation()
+        except Exception as e:
+            print("Error in reconciler loop:", e)
+
+        time.sleep(INTERVAL)
+
+def reconciliation():
     db_filter = get_filter_db.main()
     tc_filter = get_filter_tc.parse_filters(get_filter_tc.get_tc_filters())
     add_list,delete_list= compare_ac.compare(db_filter,tc_filter)
 
     print(f"del:{delete_list}")
     print(f"add:{add_list}")
-    
+
     for entry in delete_list:
         print("delete")
         tc_executer.delete_filter(entry['handle'], 0)
@@ -24,4 +36,4 @@ def main():
         )
 
 if __name__=="__main__":
-    main()
+    reconciliation_loop()
